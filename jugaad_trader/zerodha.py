8e2329e4c3065fedc96d978f9b58f089ae4467bc
kshib <ksyko@pm.me>
2023-05-19 15:58:16 +0530

    revert change to match parent's signature

--- jugaad_trader/zerodha.py
+++ jugaad_trader/zerodha.py
@@ -50,7 +50,7 @@ class Zerodha(KiteConnect):
         self.url_patch = '/oms'
         # self._routes["user.profile"] = "/user/profile/full"
 
-    def set_access_token(self, _):
+    def set_access_token(self):
         self.load_session()
 
     def load_session(self, path=None):
