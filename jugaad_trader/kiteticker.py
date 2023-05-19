3240b328a0cf4f2d08a79ca1a8afbc2e6370e17a
kshib <ksyko@pm.me>
2023-05-19 15:33:18 +0530

    add missing params

--- jugaad_trader/kiteticker.py
+++ jugaad_trader/kiteticker.py
@@ -1,31 +1,34 @@
 from kiteconnect import KiteTicker
 
+from jugaad_trader.zerodha import log
+
 
 class ZerodhaKiteTicker(KiteTicker):
-    def __init__(self, api_key, access_token, debug=False, root=None,
-                 reconnect=True, reconnect_max_tries=RECONNECT_MAX_TRIES, reconnect_max_delay=RECONNECT_MAX_DELAY,
-                 connect_timeout=CONNECT_TIMEOUT):
+
+    def __init__(self, api_key, access_token, debug=False, root=None, reconnect=True):
+        super().__init__(api_key, access_token, debug, root, reconnect)
+
         self.root = root or self.ROOT_URI
 
         # Set max reconnect tries
-        if reconnect_max_tries > self._maximum_reconnect_max_tries:
+        if super().reconnect_max_tries > self._maximum_reconnect_max_tries:
             log.warning(
                 "`reconnect_max_tries` can not be more than {val}. Setting to highest possible value - {val}.".format(
                     val=self._maximum_reconnect_max_tries))
             self.reconnect_max_tries = self._maximum_reconnect_max_tries
         else:
-            self.reconnect_max_tries = reconnect_max_tries
+            self.reconnect_max_tries = super().reconnect_max_tries
 
         # Set max reconnect delay
-        if reconnect_max_delay < self._minimum_reconnect_max_delay:
+        if super().reconnect_max_delay < self._minimum_reconnect_max_delay:
             log.warning(
                 "`reconnect_max_delay` can not be less than {val}. Setting to lowest possible value - {val}.".format(
                     val=self._minimum_reconnect_max_delay))
             self.reconnect_max_delay = self._minimum_reconnect_max_delay
         else:
-            self.reconnect_max_delay = reconnect_max_delay
+            self.reconnect_max_delay = super().reconnect_max_delay
 
-        self.connect_timeout = connect_timeout
+        self.connect_timeout = super().connect_timeout
 
         self.socket_url = "{root}?api_key={api_key}" \
                           "&enctoken={access_token}".format(
